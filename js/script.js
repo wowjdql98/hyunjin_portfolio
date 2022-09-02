06269094b28dd9d5297ecbc67b4ef619596470e0
hyun_jin98 <105402290+wowjdql98@users.noreply.github.com>
2022-09-02 16:19:37 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+
+
 $(document).ready(function () {
   $('#fullpage').fullpage({
     //options here
     responsiveWidth: 1000,
-    sectionsColor: ['#fff', '#4B6CB7', '#CFDEF3','#4B6CB7'],
-    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    sectionsColor: ['#fff', '#DCD4E5', '#CFDEF3',''],
+    anchors: ['firstPage', 'secondPage', 'thirdPage','fourthPage'],
     menu: '#enu',
     navigation: true,
     navigationTooltips: ['HOME', 'ABOUT', 'PORTFOLIO', 'CONTACT'],
@@ -13,4 +15,33 @@ $(document).ready(function () {
 
 });
 
+
+// 모바일 토글메뉴
+$(function () {
+
+  $('#toggle-menu-btn').click(function () {
+      $('#toggle-menu-btn').toggleClass('active');
+      $('#toggle-menu').toggleClass('on');
+  });
+});
+
+$(document).ready(function(){
+  var stars=800;
+  var $stars=$(".stars");
+  var r=800;
+  for(var i=0;i<stars;i++){
+    var $star=$("<div/>").addClass("star");
+    $stars.append($star);
+  }
+  $(".star").each(function(){
+    var cur=$(this);
+    var s=0.2+(Math.random()*1);
+    var curR=r+(Math.random()*300);
+    cur.css({ 
+      transformOrigin:"0 0 "+curR+"px",
+      transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+       
+    })
+  })
+})
 
